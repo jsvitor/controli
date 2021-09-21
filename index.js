57eb2282315ef36eca5245335cb1026694c90367
Vitor <jszvitor@gmail.com>
2021-09-21 17:31:22 -0300

    feat(main): added index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+app.get('/foo', function (req, res) {
+  res.json({ "check": "okay"})
+})
